5070948bd60b05cbba248eb0f12d66462e1a346e
Rafael <rafaelpianaro@gmail.com>
2021-11-04 16:24:17 -0300

    *ProductController *routes

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,6 @@
 <?php
 
-use App\Models\Product;
+use App\Http\Controllers\ProductController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -15,19 +15,16 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/products', function(){
-    // return 'products';
-    return Product::all();
-});
+Route::get('/products', [ProductController::class, 'index']);
 
-Route::post('/products', function(){
-    return Product::create([
-        'name' => 'Product one',
-        'slug' => 'product_one',
-        'description' => 'This is product one',
-        'price' => '99.99',
-    ]);
-});
+// Route::post('/products', function(){
+//     return Product::create([
+//         'name' => 'Product one',
+//         'slug' => 'product_one',
+//         'description' => 'This is product one',
+//         'price' => '99.99',
+//     ]);
+// });
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
